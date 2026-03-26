03b3ce0d5b8a9f4453819db4aded3ccf924375f6
Truong Duc Hoang <tantiennguyen2404@gmail.com>
2026-03-27 06:20:26 +0700

    Complete the function of uploading file (avatar) to the server (webapp/resources/images/avatar)

--- src/main/java/com/tdhoang/springdemo/controller/admin/UserController.java
+++ src/main/java/com/tdhoang/springdemo/controller/admin/UserController.java
@@ -1,9 +1,5 @@
 package com.tdhoang.springdemo.controller.admin;
 
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
@@ -13,23 +9,22 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.tdhoang.springdemo.model.User;
+import com.tdhoang.springdemo.service.UploadFileService;
 import com.tdhoang.springdemo.service.UserService;
 
-import jakarta.servlet.ServletContext;
 
 @Controller
 public class UserController {
     private final UserService userService;
-    private final ServletContext servletContext;
+    private final UploadFileService uploadFileService;
 
-    public UserController(UserService userService, ServletContext servletContext) {
+    public UserController(UserService userService, UploadFileService uploadFileService) {
         this.userService = userService;
-        this.servletContext = servletContext;
+        this.uploadFileService = uploadFileService;
     }
 
     @RequestMapping("/")
@@ -87,25 +82,7 @@ public class UserController {
     public String createUser(Model model,
             @ModelAttribute("newUser") User user,
             @RequestParam("avatar_file") MultipartFile file) {
-        
-        try {
-            byte[] bytes = file.getBytes();
-
-            String rootPath = this.servletContext.getRealPath("/resources/images");
-            File dir = new File(rootPath + File.separator + "avatar");
-            if (!dir.exists())
-                dir.mkdirs();
-            // Create the file on server
-            File serverFile = new File(dir.getAbsolutePath() + File.separator +
-                    +System.currentTimeMillis() + "-" + file.getOriginalFilename());
-            BufferedOutputStream stream = new BufferedOutputStream(
-                    new FileOutputStream(serverFile));
-            stream.write(bytes);
-            stream.close();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+        String avatar = this.uploadFileService.handleSaveUploadFile(file, "avatar");
         // this.userService.handleSaveUser(user);
         return "redirect:/admin/users";
     }
